7d7d553980542af11e20efe3d3459db64bddb1ad
danielchowyc <daniellchow@gmail.com>
2020-09-14 19:48:58 +0100

    made comment schema

---
+++ models/commentModel.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var CommentSchema = new Schema(
+    {
+      body: {type: String, required: true},
+      author: {type: String, required: true},
+      post: {type: Schema.Types.ObjectId, ref: "Post", required: true },
+      date: {type: Date, default: Date.now}
+    }
+);
+
+// Virtual for date formatted
+PostSchema
+.virtual('date_formatted')
+.get(function () {
+    return moment.this.date.format('MMMM Do, YYYY');
+});
+
+// Export model
+module.export = mongoose.model("Comment", CommentSchema);
